1af05cf02c607c5ee0897b15e970f5545d5d4996
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-30 21:37:36 +0000

    controller作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  # 管理者用
+  devise_for :admins, skip: [:registrations, :passwords] , controllers: {
+  sessions: "admin/sessions"
+}
+  
+  # 会員用
+  devise_for :users, skip: [:passwords] , controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
