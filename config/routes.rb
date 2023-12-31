390caf5dae74ebd4caa7f7fc2dbefdaa40924895
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-31 07:37:05 +0000

    routes編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,26 @@
 Rails.application.routes.draw do
+  
+  # 管理者用
+  devise_for :admins, skip: [:registrations, :passwords] , controllers: {
+  sessions: "admin/sessions"
+}
 
+  # 会員用
+  devise_for :users, skip: [:passwords] , controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+   # 管理者用
   namespace :admin do
-    get 'searches/search'
-    get 'searches/index'
+    get '/admin', to: 'homes#top'
   end
-  namespace :user do
-    get 'searches/search'
-    get 'searches/index'
-  end
-  namespace :user do
-    get 'comments/create'
-    get 'comments/edit'
-    get 'comments/update'
-    get 'comments/destroy'
-  end
-  namespace :user do
-    get 'favorites/create'
-    get 'favorites/destroy'
-    get 'favorites/index'
-  end
-  namespace :user do
-    get 'posts/new'
-    get 'posts/edit'
-    get 'posts/update'
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :user do
+  namespace :admin do
+    get 'users/index'
     get 'users/show'
     get 'users/edit'
-    get 'users/update'
-  end
-  namespace :user do
-    get 'homes/top'
+    get 'users/updete'
   end
   namespace :admin do
     get 'posts/index'
@@ -45,23 +33,96 @@ Rails.application.routes.draw do
     get 'comments/destroy'
   end
   namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/updete'
+    get 'searches/search'
+    get 'searches/index'
   end
-  namespace :admin do
+  
+  # 会員用
+  namespace :user do
     get 'homes/top'
   end
-  # 管理者用
-  devise_for :admins, skip: [:registrations, :passwords] , controllers: {
-  sessions: "admin/sessions"
-}
-
-  # 会員用
-  devise_for :users, skip: [:passwords] , controllers: {
-  registrations: "user/registrations",
-  sessions: 'user/sessions'
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :user do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
+  namespace :user do
+    get 'posts/new'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/index'
+    get 'posts/show'
+  end
+  namespace :user do
+    get 'comments/create'
+    get 'comments/edit'
+    get 'comments/update'
+    get 'comments/destroy'
+  end
+  namespace :user do
+    get 'searches/search'
+    get 'searches/index'
+  end
+  namespace :user do
+    get 'favorites/create'
+    get 'favorites/destroy'
+    get 'favorites/index'
+  end
+  
+  
+  
+  
+  # namespace :admin do
+  #   get 'searches/search'
+  #   get 'searches/index'
+  # end
+  # namespace :user do
+  #   get 'searches/search'
+  #   get 'searches/index'
+  # end
+  # namespace :user do
+  #   get 'comments/create'
+  #   get 'comments/edit'
+  #   get 'comments/update'
+  #   get 'comments/destroy'
+  # end
+  # namespace :user do
+  #   get 'favorites/create'
+  #   get 'favorites/destroy'
+  #   get 'favorites/index'
+  # end
+  # namespace :user do
+  #   get 'posts/new'
+  #   get 'posts/edit'
+  #   get 'posts/update'
+  #   get 'posts/index'
+  #   get 'posts/show'
+  # end
+  # namespace :user do
+  #   get 'users/show'
+  #   get 'users/edit'
+  #   get 'users/update'
+  # end
+  # namespace :user do
+  #   get 'homes/top'
+  # end
+  # namespace :admin do
+  #   get 'posts/index'
+  #   get 'posts/show'
+  #   get 'posts/destroy'
+  # end
+  # namespace :admin do
+  #   get 'comments/index'
+  #   get 'comments/show'
+  #   get 'comments/destroy'
+  # end
+  # namespace :admin do
+  #   get 'users/index'
+  #   get 'users/show'
+  #   get 'users/edit'
+  #   get 'users/updete'
+  # end
+  # namespace :admin do
+  #   get 'homes/top'
+  # end
 end
