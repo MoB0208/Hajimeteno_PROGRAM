84c203fef7f8fa1a3b020612c7118216c5f328b3
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-12-31 18:19:50 +0000

    top画面作成中

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomesController < ApplicationController
   def top
+    @posts = Post.page(params[:page]).per(20).order(created_at: :desc)
+    # 20件ごとに新着順で表記
+    @genres = Genres.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # 管理者用
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
   sessions: "admin/sessions"
@@ -11,16 +11,20 @@ Rails.application.routes.draw do
   sessions: 'user/sessions'
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
    # 管理者用
   namespace :admin do
     get '/admin', to: 'homes#top'
   end
   namespace :admin do
-    get '/admin/users', to: 'users#index'
-    get '/admin/users/:id', to: 'users#show'
-    get '/admin/users/:id/edit', to: 'users#edit'
+    get '/admin/users', to: 'users#index', as: 'users_index'
+    get '/admin/users/:id', to: 'users#show', as: 'users_show'
+    get '/admin/users/:id/edit', to: 'users#edit', as: 'users_edit'
     patch 'users/update'
+  end
+  namespace :admin do
+    get '/admin/genres', to: 'genres#index', as: 'genres_index'
+    get '/admin/genres/:id', to: 'genres#show', as: 'genres_show'
   end
   namespace :admin do
     get '/admin/posts', to: 'posts#index'
@@ -36,43 +40,47 @@ Rails.application.routes.draw do
     get '/admin/searches', to: 'searches#search'
     get '/admin/searches', to: 'searches#index'
   end
-  
-  
+
+
   # 会員用
   namespace :user do
-    get 'homes/top'
+    root to: 'homes#top'
   end
   namespace :user do
-    get 'users/show'
-    get 'users/edit'
+    get '/home', to: 'users#show'
+    get '/infomation/edit', to: 'users#edit'
     patch 'users/update'
   end
   namespace :user do
-    get 'posts/new'
-    get 'posts/edit'
+    get '/genres/:id', to: 'genres#index', as: 'genres_index'
+    get '/genres/:id', to: 'genres#show', as: 'genres_show'
+  end
+  namespace :user do
+    get '/posts/new', to: 'posts#new'
+    get '/posts/:id/edit', to: 'posts#edit'
     patch 'posts/update'
-    get 'posts/index'
-    get 'posts/show'
+    get '/posts', to: 'posts#index'
+    get '/posts/:id', to: 'posts#show'
   end
   namespace :user do
     post 'comments/create'
-    get 'comments/edit'
+    get '/comments/:id', to: 'comments#edit'
     patch 'comments/update'
     delete 'comments/destroy'
   end
   namespace :user do
-    get 'searches/search'
-    get 'searches/index'
+    get '/searches', to: 'searches#search'
+    get '/searches', to: 'searches#index'
   end
   namespace :user do
     post 'favorites/create'
     delete 'favorites/destroy'
-    get 'favorites/index'
+    get '/favorites', to: 'favorites#index'
   end
-  
-  
-  
-  
+
+
+
+
   # namespace :admin do
   #   get 'searches/search'
   #   get 'searches/index'
