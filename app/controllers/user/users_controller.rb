26cb1e497d7c22d27e30f122eebaecdddf4124da
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-02 20:31:54 +0000

    ホーム・会員情報編集controller作成

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,15 +1,25 @@
 class User::UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
+    @user = current_user
+    @favorite_posts = current_user.favorite.post.all
+    @user_post = current_user.post.all
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_infomation_edit_path(@user), notice: "編集内容を保存しました。"
+    else
+      render user_infomation_edit_path(@user)
+    end
   end
-  
+
   private
 
   def user_params
