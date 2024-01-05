04223bfec8cc66c904556eb99721f16a1fac729b
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-05 18:15:44 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 管理者用
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
@@ -10,71 +11,71 @@ Rails.application.routes.draw do
   registrations: "user/registrations",
   sessions: 'user/sessions'
 }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
    # 管理者用
-  namespace :admin do
-    #get '/admin', to: 'homes#top'
+  namespace :admins do
+    # Top画面
     root to: 'homes#top'
-  end
-  namespace :admin do
-    get '/admin/users', to: 'users#index', as: 'users_index'
-    get '/admin/users/:id', to: 'users#show', as: 'users_show'
-    get '/admin/users/:id/edit', to: 'users#edit', as: 'users_edit'
+
+    # 会員一覧・管理関連
+    get '/users', to: 'users#index', as: 'users_index'
+    get '/users/:id', to: 'users#show', as: 'users_show'
+    get '/users/:id/edit', to: 'users#edit', as: 'users_edit'
     patch 'users/update'
-  end
-  namespace :admin do
-    get '/admin/genres', to: 'genres#index', as: 'genres_index'
-    get '/admin/genres/:id', to: 'genres#show', as: 'genres_show'
-  end
-  namespace :admin do
-    get '/admin/posts', to: 'posts#index', as: 'posts_index'
-    get '/admin/posts/:id', to: 'posts#show', as: 'posts_show'
+
+    # 言語一覧
+    get '/genres', to: 'genres#index', as: 'genres_index'
+    get '/genres/:id', to: 'genres#show', as: 'genres_show'
+
+    # 投稿関連
+    get '/posts', to: 'posts#index', as: 'posts_index'
+    get '/posts/:id', to: 'posts#show', as: 'posts_show'
     delete 'posts/destroy'
-  end
-  namespace :admin do
-    get '/admin/comments', to: 'comments#index', as: 'comments_index'
-    get '/admin/comments/:id', to: 'comments#show', as: 'comments_show'
+
+    # コメント関連
+    get '/comments', to: 'comments#index', as: 'comments_index'
+    get '/comments/:id', to: 'comments#show', as: 'comments_show'
     delete 'comments/destroy'
-  end
-  namespace :admin do
-    get "search" => 'searches#search'
-    get '/admin/searches', to: 'searches#index', as: 'searches_index'
+
+    # 検索
+    get "/search" => 'searches#search'
+    get '/searches', to: 'searches#index', as: 'searches_index'
   end
 
 
   # 会員用
-  #namespace :user do
-    root to: 'user/homes#top'
-  #end
-  namespace :user do
+  # Top画面
+  root to: 'user/homes#top'
+
+  scope module: :users do
+    # ホーム
     get '/home', to: 'users#show'
     get '/infomation/edit', to: 'users#edit'
     patch 'users/update'
-  end
-  namespace :user do
+
+    # 言語一覧
     get '/genres/:id', to: 'genres#index', as: 'genres_index'
     get '/genres/:id', to: 'genres#show', as: 'genres_show'
-  end
-  namespace :user do
+
+    # 投稿関連
     get '/posts/new', to: 'posts#new', as: 'posts_new'
     post '/posts/create' => 'posts#create'
     get '/posts/:id/edit', to: 'posts#edit', as: 'posts_edit'
     patch 'posts/update'
     get '/posts', to: 'posts#index', as: 'posts_index'
     get '/posts/:id', to: 'posts#show', as: 'posts_show'
-  end
-  namespace :user do
+
+    # コメント関連
     post 'comments/create'
     get '/comments/:id', to: 'comments#edit', as: 'comments_edit'
     patch 'comments/update'
     delete 'comments/destroy'
-  end
-  namespace :user do
+
+    # 検索
     get "search" => 'searches#search'
     get '/searches', to: 'searches#index', as: 'searches_index'
-  end
-  namespace :user do
+
+    # お気に入り
     post 'favorites/create'
     delete 'favorites/destroy'
     get '/favorites', to: 'favorites#index', as: 'favorites_index'
