39bfff1d06ea250ddc71f4e7c83b29393c5fe559
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-05 16:17:19 +0000

    top画面エラー解消

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,8 +1,10 @@
 class User::HomesController < ApplicationController
   def top
-    @genres = Genres.all
-    @favorite_posts = Favorite.post.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
-    @posts = Post.page(params[:page]).per(20).order(created_at: :desc)
-    # 20件ごとに新着順で表記
+    @genres = Genre.all
+    @favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    # お気に入りが多い順の投稿一覧
+    #@favorite_posts = Favorite.post.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
+    @posts = Post.order('id DESC').limit(20)
+    # 新着20件
   end
 end
