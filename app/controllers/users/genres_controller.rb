6bae75318fde20fb16f45c04d65b43c7dde760ab
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-12 17:56:00 +0000

    会員側レイアウト調整3

--- app/controllers/users/genres_controller.rb
+++ app/controllers/users/genres_controller.rb
@@ -7,4 +7,14 @@ class Users::GenresController < ApplicationController
 
   def show
   end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :main_code)
+  end
+
+  def genre_params
+    params.require(:genre).permit(:genre_name, :version)
+  end
 end
--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -49,8 +49,14 @@ class Users::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :main_code,:genre_name, :version,:code, :body)
-    # params.require(:genre).permit(:genre_name, :version)
-    # params.require(:post_code).permit(:code, :body)
+    params.require(:post).permit(:title, :main_code)
+  end
+
+  def genre_params
+    params.require(:genre).permit(:genre_name, :version)
+  end
+
+  def post_code_params
+    params.require(:post_code).permit(:code, :body)
   end
 end
