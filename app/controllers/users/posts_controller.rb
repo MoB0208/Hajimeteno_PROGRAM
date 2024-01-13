1126333d3fd73f3989b1a115f7e300887e8a7f9b
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-13 05:56:04 +0000

    投稿機能エラー対応中

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -3,15 +3,19 @@ class Users::PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post_code = PostCode.new
+    @genre = Genre.new
   end
 
   def create
     @user = current_user
     @post = Post.new(post_params)
+    @post_code = PostCode.new(post_code_params)
+    @genre = Genre.new(genre_params)
     @post.user_id = current_user.id
     if @post.save
       puts @post.errors.full_messages
-      redirect_to user_home_path(@user), notice: "投稿しました。"
+      redirect_to users_home_path(@user), notice: "投稿しました。"
     else
       @posts = Post.all
       render :new
@@ -27,7 +31,7 @@ class Users::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to user_home_path(@user), notice: "投稿を修正しました。"
+      redirect_to users_home_path(@user), notice: "投稿を修正しました。"
     else
       render :edit
     end
@@ -52,11 +56,11 @@ class Users::PostsController < ApplicationController
     params.require(:post).permit(:title, :main_code)
   end
 
-  def genre_params
-    params.require(:genre).permit(:genre_name, :version)
-  end
-
   def post_code_params
     params.require(:post_code).permit(:code, :body)
+  end
+
+  def genre_params
+    params.require(:genre).permit(:genre_name, :version)
   end
 end
