a5b547703cfcbeca8b6ee0ab15f5723b45759fa2
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-18 19:55:44 +0000

    編集画面エラー対応中2

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -38,32 +38,29 @@ class Users::PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
     @genre = @post.genres
-    # # .find(
-    # #   genre_name: params[:post][:genre][:genre_name],
-    # #   version: params[:post][:genre][:version],
-    # # )
     @content = @post.contents
-    # @main_text = @post.main_texts
-    # @post_code = @post.post_codes
+    @main_text = @post.main_texts
+    @post_code = @post.post_codes
   end
 
   def update
     @post = Post.find(params[:id])
-    @genre = @post.genres.build(
-      genre_name: params[:post][:genre][:genre_name],
-      version: params[:post][:genre][:version],
-    )
-    @content = @post.contents.build(
-      table_of_content: params[:post][:content][:table_of_content],
-    )
-    @main_text = @post.main_texts.build(
-      body: params[:post][:main_text][:body],
-    )
-    @post_code = @post.post_codes.build(
-      position: params[:post][:post_code][:position],
-      code: params[:post][:post_code][:code],
-    )
+
     if @post.update(post_params)
+       @genre = @post.genres.update(
+         genre_name: params[:post][:genre][:genre_name],
+         version: params[:post][:genre][:version],
+       )
+       @content = @post.contents.update(
+         table_of_content: params[:post][:content][:table_of_content],
+       )
+       @main_text = @post.main_texts.update(
+         body: params[:post][:main_text][:body],
+       )
+       @post_code = @post.post_codes.update(
+         position: params[:post][:post_code][:position],
+         code: params[:post][:post_code][:code],
+       )
       redirect_to home_path, notice: "記事を修正しました。"
     else
       render :edit, notice: "記事の保存に失敗しました。"
