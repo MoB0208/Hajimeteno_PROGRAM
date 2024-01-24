1c71061a6667e73c525970c479fef3ba632fb760
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-01-24 11:57:46 +0000

    関連記事機能追加

--- app/controllers/users/genres_controller.rb
+++ app/controllers/users/genres_controller.rb
@@ -5,17 +5,5 @@ class Users::GenresController < ApplicationController
     genre_id = params[:genre_id]
     genre_posts = GenrePost.where(genre_id: genre_id).pluck(:post_id)
     @posts = Post.where(id: genre_posts)
-    # @posts = Post.where(genre_post_id: @genre.id).all
-    # @posts = Post.where(genre_name: params[:id]).order(:main_code)
   end
-
-  # private
-
-  # def post_params
-  #   params.require(:post).permit(:title, :main_code, :version)
-  # end
-
-  # def genre_params
-  #   params.require(:genre).permit(:genre_name)
-  # end
 end
