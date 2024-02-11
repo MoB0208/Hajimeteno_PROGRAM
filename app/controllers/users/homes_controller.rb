3d0cf620a739d49647a252070988d7a344733498
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2024-02-11 17:41:41 +0000

    エラー解消

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -3,7 +3,7 @@ class Users::HomesController < ApplicationController
     @genres = Genre.all
     # お気に入りが多い順の投稿一覧
     @favorite_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
-    # @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page]).per(10)
+    @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page]).per(5)
     # 新着20件
     @posts = Post.order('id DESC').limit(20)
   end
